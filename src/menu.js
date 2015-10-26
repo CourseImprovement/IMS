003138dcfe400bd6e780ba96f931ff8abd036030
= <chase.willden@gmail.com>
2015-10-26 16:24:19 -0600

    added menu

---
+++ src/menu.js
@@ -0,0 +1,28 @@
+/**
+ * ary = [
+ * 		{
+ * 			value: 'FA15',
+ * 			href: 'laskjdflskf.aspx'
+ * 		}
+ * ]
+ * @param {[type]} obj [description]
+ */
+function Menu(ary){
+
+}
+
+Menu.prototype.click = function(callback){
+
+}
+
+Menu.prototype.getItem = function(idx){
+	
+}
+
+function MenuItem(obj){
+
+}
+
+MenuItem.prototype.click = function(callback){
+
+}
