09771849a8050c39e3383a3ad6d094383ff9c793
= <chase.willden@gmail.com>
2015-11-11 16:36:18 -0700

    working on rollup

--- build/admin.js
+++ build/admin.js
@@ -1276,8 +1276,7 @@ Rollup.prototype.update = function(){
 		'Inspire a Love for Learning',
 		'Develop Relationships with and among Students',
 		'Embrace University Citizenship',
-		'Building Faith in Jesus Christ',
-		'Weekly Hours'
+		'Building Faith in Jesus Christ'
 	]
 	$(window.config._xml).find('semester[code=' + window.config.getCurrentSemester() + '] survey[id=' + this._surveyId + '] question').each(function(){
 		for (var i = 0; i < questions.length; i++){
@@ -1302,10 +1301,48 @@ Rollup.prototype.update = function(){
 			var text = $(this).find('survey[id=' + _this._surveyId + '] answer[id=' + _this._questions[i].id + ']').text();
 			if (text.length == 0) continue;
 			if (!result[_this._questions[i].id][leader]) result[_this._questions[i].id][leader] = [];
-			result[_this._questions[i].id][leader].push(text);
+			result[_this._questions[i].id][leader].push(parseFloat(text));
 		}
 	});
-	console.log(result);	
+
+	var top = {};
+	var aims = {};
+
+	for (var q in result){
+		top[q] = {total: 0, sum: 0}
+		for (var tgl in result[q]){
+			var ary = result[q][tgl];
+			var isAim = $(master).find('semester[code=' + window.config.getCurrentSemester() + '] > people > person[email=' + tgl + ']').attr('highestrole') == 'aim';
+			if (isAim){
+				if (!aims[q]) aims[q] = {};
+				if (!aims[q][tgl]) aims[q][tgl] = [];
+				aims[q][tgl].concat(result[q][tgl].raw);
+			}
+			var count = ary.length;
+			var sum = 0;
+			for (var i = 0; i < count; i++){
+				sum += ary[i];
+				top[q].sum += ary[i];
+				top[q].total++;
+			}
+			var avg = sum / count;
+			avg = Math.floor(avg * 10) / 10;
+			$(this._xml).find('semester[code=' + window.config.getCurrentSemester() + '] person[email=' + tgl + '][type=tgl] question[name="' + questions[q] + '"]').append('<survey id="' + this._surveyId + '" value="' + avg + '" />');
+		}
+		for (var aim in aims[q]){
+			var ary = aims[q][aim];
+			var count = ary.length;
+			var sum = 0;
+			for (var i = 0; i < count; i++){
+				sum += ary[i];
+			}
+			var avg = sum / count;
+			avg = Math.floor(avg * 10) / 10;
+
+			$(this._xml).find('semester[code=' + window.config.getCurrentSemester() + '] person[email=' + tgl + '][type=tgl] question[name="' + questions[q] + '"]').append('<survey id="' + this._surveyId + '" value="' + avg + '" />');
+		}
+		$(this._xml).find('semester[code=' + window.config.getCurrentSemester() + '] > questions > question[name="' + questions[q] + '"]').append('<survey id="' + this._surveyId + '" value="' + avg + '" />');
+	}
 }
 // GROUP ROLLUP END
 
