e228642c19d4778c17280e9888be94191194144e
Lonestar421 <grantjsheridan@gmail.com>
2015-11-11 14:45:55 -0700

    email check in survey process()

--- src/admin/survey.js
+++ src/admin/survey.js
@@ -145,8 +145,7 @@ Survey.prototype.process = function(rows){
 	}
 	for (var i = spot; i < rows[i].length; i++){
 		// clean answers  and then add them to their respective individual
-		if (rows[i].length > 1){
-			console.log(rows[i][eCol]);
+		if (rows[i][eCol] != undefined){
 			var person = this.getPerson(rows[i][eCol]);
 			var again = false;
 			if (!person){
