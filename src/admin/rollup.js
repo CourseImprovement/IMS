6fbf1139b82a2cd59b583c7a63859c94c9c83cd8
Lonestar421 <grantjsheridan@gmail.com>
2015-11-12 10:31:14 -0700

    bug fix

--- src/admin/rollup.js
+++ src/admin/rollup.js
@@ -156,7 +156,7 @@ Rollup.prototype.aimLevelUpdate = function(){
 		for (var q in result[a]){
 			var total = result[a][q].length;
 			var sum = 0;
-			var question = $(window._rollup).find('semester[code=' + window.config.getCurrentSemester() + '] people > person[email=' + a + '] question[name="' + questions[q] + '"]');
+			var question = $(window._rollup).find('semester[code=' + window.config.getCurrentSemester() + '] people > person[email=' + a + '][type=aim] question[name="' + questions[q] + '"]');
 			for (var i = 0; i < result[a][q].length; i++){
 				sum += result[a][q][i];
 			}
