9706593136622b92449809a8d87c319c8a701998
= <chase.willden@gmail.com>
2015-11-12 10:50:33 -0700

    test

--- build/admin.js
+++ build/admin.js
@@ -1729,6 +1729,7 @@ Survey.prototype.process = function(rows){
 				continue;
 			}
 			var again = false;
+			var oldPlacement;
 			if (!person){
 				person = new Person({
 					email: rows[i][eCol],
@@ -1736,11 +1737,14 @@ Survey.prototype.process = function(rows){
 					placement: this.placement,
 					answers: Answer.collect(this, rows[i])
 				}, false, true);
+				oldPlacement = person._placement;
 			}
 			else{
 				person._answers = Answer.collect(this, rows[i]);
 				person._row = rows[i];
 				again = true;
+				oldPlacement = person._placement;
+				person._placement = this.placement;
 			}
 			if (cCol != -1){
 				person.course = Survey.cleanCourse(rows[i][cCol]);
@@ -1749,6 +1753,7 @@ Survey.prototype.process = function(rows){
 				if (!again) this.people.push(person);
 				person.process();
 				this.processed++;
+				person._placement = oldPlacement;
 			}
 			else{
 				console.log('Invalid person: ' + rows[i][eCol]);
