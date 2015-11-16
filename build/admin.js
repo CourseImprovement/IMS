81a93bb9488048baa1ce30be67c652770275e2a4
= <chase.willden@gmail.com>
2015-11-16 14:31:35 -0700

    fixed break

--- build/admin.js
+++ build/admin.js
@@ -2229,6 +2229,7 @@ Survey.prototype.process = function(rows){
 			window.rollup.update();
 			ims.loading.reset();
 			alert('Complete');
+			return;
 		} 
 		// clean answers  and then add them to their respective individual
 		if (rows[i][eCol] != undefined){
