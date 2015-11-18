d842faeaa51b16b27e5b75e516e3432ea25ed4cd
= <chase.willden@gmail.com>
2015-11-18 13:47:08 -0700

    fixed upper lower

--- build/admin.js
+++ build/admin.js
@@ -388,14 +388,14 @@ function MasterPerson(xml, master){
  */
 MasterPerson.prototype.addUpperAndLowers = function(){
 	var _this = this;
-	$(this._xml).find('> roles > role[type=' + this.highestRole + '] > stewardship > people > person').each(function(){
+	$(this._xml).find('> roles > role > stewardship > people > person').each(function(){
 		var person = _this.master.graph[$(this).attr('email')];
 		_this.lowers.push({
 			role: $(this).attr('type'),
 			person: person
 		});
 	});
-	$(this._xml).find('> roles > role[type=' + this.highestRole + '] > leadership > person').each(function(){
+	$(this._xml).find('> roles > role > leadership > person').each(function(){
 		var person = _this.master.graph[$(this).attr('email')];
 		_this.leaders[$(this).attr('type')] = person;
 		_this.uppers.push({
@@ -1590,7 +1590,20 @@ Evaluations.prototype.parseCSV = function(){
 function Permissions(){
 	console.log('new Permissions object created');
 	this._xml = this.getPermissionsXml();
-	var map = window.config.getMaster();
+	//this.map = window.config.getMaster();
+	this.map = new Master();
+	this.init();
+}
+
+Permissions.prototype.init = function(){
+	this.graph = {};
+	this.people = [];
+	var _this = this;
+	$(this._xml).find('file').each(function(){
+		var p = new PermissionsPerson(this, _this);
+		_this.people.push(p);
+		_this.graph[p.email] = p;
+	});
 }
 
 Permissions._xml = null;
@@ -1606,8 +1619,14 @@ Permissions.prototype.getPermissionsXml = function(){
  * CHECK IF THERE ARE ANY CHANGES TO DO
  */
 Permissions.prototype.check = function(){
-	if (!this._areDifferent) return true;
 	console.log('Checking if permissions need changing');
+	var add = [];
+	var remove = [];
+	for (var i = 0; i < this.people.length; i++){
+		if (this.people[i].access.length == this.map.graph[this.people[i].email].uppers.length){
+			console.log('fine');
+		}
+	}
 }
 
 /**
@@ -1617,6 +1636,19 @@ Permissions.prototype.update = function(){
 	console.log('updating the permissions');
 }
 // GROUP PERM             ISSIONS END
+
+function PermissionsPerson(xml, permissions){
+	this.email = $(xml).attr('email');
+	this.access = [];
+	this.permissions = permissions;
+	if ($(xml).prop('nodeName') == 'file'){
+		var _this = this;
+		this.email = $(xml).attr('name');
+		$(xml).find('user').each(function(){
+			_this.access.push(new PermissionsPerson(this, permissions));
+		})
+	}
+}
 
 
 
--- src/admin/permissions.js
+++ src/admin/permissions.js
@@ -7,7 +7,20 @@
 function Permissions(){
 	console.log('new Permissions object created');
 	this._xml = this.getPermissionsXml();
-	var map = window.config.getMaster();
+	//this.map = window.config.getMaster();
+	this.map = new Master();
+	this.init();
+}
+
+Permissions.prototype.init = function(){
+	this.graph = {};
+	this.people = [];
+	var _this = this;
+	$(this._xml).find('file').each(function(){
+		var p = new PermissionsPerson(this, _this);
+		_this.people.push(p);
+		_this.graph[p.email] = p;
+	});
 }
 
 Permissions._xml = null;
@@ -23,8 +36,14 @@ Permissions.prototype.getPermissionsXml = function(){
  * CHECK IF THERE ARE ANY CHANGES TO DO
  */
 Permissions.prototype.check = function(){
-	if (!this._areDifferent) return true;
 	console.log('Checking if permissions need changing');
+	var add = [];
+	var remove = [];
+	for (var i = 0; i < this.people.length; i++){
+		if (this.people[i].access.length == this.map.graph[this.people[i].email].uppers.length){
+			console.log('fine');
+		}
+	}
 }
 
 /**
@@ -34,3 +53,16 @@ Permissions.prototype.update = function(){
 	console.log('updating the permissions');
 }
 // GROUP PERM             ISSIONS END
+
+function PermissionsPerson(xml, permissions){
+	this.email = $(xml).attr('email');
+	this.access = [];
+	this.permissions = permissions;
+	if ($(xml).prop('nodeName') == 'file'){
+		var _this = this;
+		this.email = $(xml).attr('name');
+		$(xml).find('user').each(function(){
+			_this.access.push(new PermissionsPerson(this, permissions));
+		})
+	}
+}
