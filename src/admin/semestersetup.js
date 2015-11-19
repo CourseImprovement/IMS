c70c953c0a5dc8e69ab299675d0220492a8f5ac1
Lonestar421 <grantjsheridan@gmail.com>
2015-11-19 14:47:40 -0700

    semester setup

--- src/admin/semestersetup.js
+++ src/admin/semestersetup.js
@@ -31,11 +31,27 @@ function OSMPerson(obj){
 }
 
 OSMPerson.prototype.addCourse = function(course){
-
+	for (var i = 0; i < this._courses.length; i++){
+		if (this._courses[i].name == course.name){
+			if (course.section != undefined){
+				this._courses[i].section += ' ' + course.section;
+			}
+			else{
+				this._courses[i]._pwsection += ' ' + course.section;
+			}
+			return;
+		}
+	}
+	this._courses.push(new Course(course));
 }
 
 OSMPerson.prototype.addRole = function(role){
-
+	for (var i = 0; i < this._roles.length; i++){
+		if (this._roles[i] == role){
+			return;
+		}
+	}
+	this._roles.push(role);
 }
 
 function Course(obj){
@@ -64,6 +80,96 @@ SemesterSetup.prototype.semesterSetup = function(){
  */
 SemesterSetup.prototype._createOrg = function(){
 	console.log('rollup is being created');
+	this._org['IM'] = [];
+	this._org['OCRM'] = [];
+
+	var start = 0;
+	while (this._csv[start][2] != 'email'){
+		start++;
+	}
+	//    0         1       2       3             4                5               6        7        8       9      10      11      12     13       14       15         16          17    18      19       20
+	//firstname|lastname|email|coursenumber|totalCredits|JenzabarSectionNumber|TglEmail|TglName|AimEmail|AimName|ImEmail|ImName|OcrEmail|OcrName|OcrmName|OcrmEmail|isNewTeacher|isPilot|IsOcr|isPathway|Semester
+
+	for (var rows = ++start; rows < this._csv.length; rows++){
+		// INSTRUCTOR OBJECT
+		var inst = {
+			first: this._csv[rows][0],
+			last: this._csv[rows][1],
+			email: this._csv[rows][2],
+			isNew: this._csv[rows][16],
+			role: 'instructor',
+			course: {
+				name: this._csv[rows][3],
+				credits: this._csv[rows][4],
+				isPilot: this._csv[rows][17]
+			},
+			stewardship: null
+		};
+
+		if (this._csv[rows][19] == 'TRUE'){
+			inst.course['pwsection'] = this._csv[rows][5];
+			inst.course['section'] = '';
+		}
+		else{
+			inst.course['section'] = this._csv[rows][5];
+			inst.course['pwsection'] = '';
+		}
+
+		// TGL OBJECT
+		var tgl = {
+			first: this._csv[rows][7].split(' ')[0],
+			last: this._csv[rows][7].split(' ')[1],
+			email: this._csv[rows][6],
+			isNew: null,
+			role: 'tgl',
+			course: null,
+			stewardship: inst
+		};
+		
+		// AIM OBJECT
+		var aim = {
+			first: this._csv[rows][9].split(' ')[0],
+			last: this._csv[rows][9].split(' ')[1],
+			email: this._csv[rows][8],
+			isNew: null,
+			role: 'aim',
+			course: null,
+			stewardship: tgl
+		};
+
+		// IM OBJECT
+		var im = new OSMPerson({
+			first: this._csv[rows][11].split(' ')[0],
+			last: this._csv[rows][11].split(' ')[1],
+			email: this._csv[rows][10],
+			isNew: null,
+			role: 'im',
+			course: null,
+			stewardship: aim
+		});
+
+		// OCR OBJECT
+		var ocr = {
+			first: this._csv[rows][13].split(' ')[0],
+			last: this._csv[rows][13].split(' ')[1],
+			email: this._csv[rows][12],
+			isNew: null,
+			role: 'ocr',
+			course: null,
+			stewardship: inst
+		};
+
+		// OCRM OBJECT
+		var ocrm = new OSMPerson({
+			first: this._csv[rows][14].split(' ')[0],
+			last: this._csv[rows][14].split(' ')[1],
+			email: this._csv[rows][15],
+			isNew: null,
+			role: 'ocrm',
+			course: null,
+			stewardship: ocr
+		});
+	}
 }
 
 /**
