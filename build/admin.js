f28b72a22e353fdefbfb30f03878dabe30d41cd5
Lonestar421 <grantjsheridan@gmail.com>
2015-11-19 16:06:04 -0700

    semester setup

--- build/admin.js
+++ build/admin.js
@@ -2249,53 +2249,53 @@ function SemesterSetup(csv){
 
 function OSMPerson(obj){
 	console.log('creating a new person');
-	this._first = obj._first;
-	this._last = obj._last;
-	this._email = obj._email;
-	this._isNew = obj._isNew;
-	this._roles = [];
-	this._roles.push(obj._role);
-	this._courses = [];
-	if (obj._course != null){
-		this._courses.push(new Course(obj._course));
+	this.first = obj.first;
+	this.last = obj.last;
+	this.email = obj.email;
+	this.isNew = obj.isNew;
+	this.roles = [];
+	this.roles.push(obj.role);
+	this.courses = [];
+	if (obj.course != null){
+		this.courses.push(new Course(obj.course));
 	}
-	this._stewardship = [];
-	if (obj._stewardship != null){
-		this._stewardship.push(new OSMPerson(obj._stewardship));
+	this.stewardship = [];
+	if (obj.stewardship != null){
+		this.stewardship.push(new OSMPerson(obj.stewardship));
 	}
 }
 
 OSMPerson.prototype.addCourse = function(course){
-	for (var i = 0; i < this._courses.length; i++){
-		if (this._courses[i].name == course.name){
+	for (var i = 0; i < this.courses.length; i++){
+		if (this.courses[i].name == course.name){
 			if (course.section != undefined){
-				this._courses[i].section += ' ' + course.section;
+				this.courses[i].section += ' ' + course.section;
 			}
 			else{
-				this._courses[i]._pwsection += ' ' + course.section;
+				this.courses[i].pwsection += ' ' + course.section;
 			}
 			return;
 		}
 	}
-	this._courses.push(new Course(course));
+	this.courses.push(new Course(course));
 }
 
 OSMPerson.prototype.addRole = function(role){
-	for (var i = 0; i < this._roles.length; i++){
-		if (this._roles[i] == role){
+	for (var i = 0; i < this.roles.length; i++){
+		if (this.roles[i] == role){
 			return;
 		}
 	}
-	this._roles.push(role);
+	this.roles.push(role);
 }
 
 function Course(obj){
 	console.log('creating a new course');
-	this._name = obj._name; 
-	this._section = obj._section; 
-	this._credits = obj._credits; 
-	this._isPilot = obj._isPilot; 
-	this._pwsection = obj._pwsection;
+	this.name = obj.name; 
+	this.section = obj.section; 
+	this.credits = obj.credits; 
+	this.isPilot = obj.isPilot; 
+	this.pwsection = obj.pwsection;
 }
 
 /**
@@ -2322,88 +2322,86 @@ SemesterSetup.prototype._createOrg = function(){
 	while (this._csv[start][2] != 'email'){
 		start++;
 	}
-	//    0         1       2       3             4                5               6        7        8       9      10      11      12     13       14       15         16          17    18      19       20
-	//firstname|lastname|email|coursenumber|totalCredits|JenzabarSectionNumber|TglEmail|TglName|AimEmail|AimName|ImEmail|ImName|OcrEmail|OcrName|OcrmName|OcrmEmail|isNewTeacher|isPilot|IsOcr|isPathway|Semester
 
 	for (var rows = ++start; rows < this._csv.length; rows++){
 		if (this._csv[rows].length == 1) continue;
 		// INSTRUCTOR OBJECT
 		var inst = {
-			_first: this._csv[rows][0],
-			_last: this._csv[rows][1],
-			_email: this._csv[rows][2],
-			_isNew: this._csv[rows][16],
-			_role: 'instructor',
-			_course: {
+			first: this._csv[rows][0],
+			last: this._csv[rows][1],
+			email: this._csv[rows][2],
+			isNew: this._csv[rows][16],
+			role: 'instructor',
+			course: {
 				name: this._csv[rows][3],
 				credits: this._csv[rows][4],
 				isPilot: this._csv[rows][17]
 			},
-			_stewardship: null
+			stewardship: null
 		};
 
 		if (this._csv[rows][19] == 'TRUE'){
-			inst._course['_pwsection'] = this._csv[rows][5];
-			inst._course['_section'] = '';
+			inst.course['pwsection'] = this._csv[rows][5];
+			inst.course['section'] = '';
 		}
 		else{
-			inst._course['_section'] = this._csv[rows][5];
-			inst._course['_pwsection'] = '';
+			inst.course['section'] = this._csv[rows][5];
+			inst.course['pwsection'] = '';
 		}
 
 		// TGL OBJECT
 		var tgl = {
-			_first: this._csv[rows][7].split(' ')[0],
-			_last: this._csv[rows][7].split(' ')[1],
-			_email: this._csv[rows][6],
-			_isNew: null,
-			_role: 'tgl',
-			_course: null,
-			_stewardship: inst
+			first: this._csv[rows][7].split(' ')[0],
+			last: this._csv[rows][7].split(' ')[1],
+			email: this._csv[rows][6],
+			isNew: null,
+			role: 'tgl',
+			course: null,
+			stewardship: inst
 		};
 		
 		// AIM OBJECT
 		var aim = {
-			_first: this._csv[rows][9].split(' ')[0],
-			_last: this._csv[rows][9].split(' ')[1],
-			_email: this._csv[rows][8],
-			_isNew: null,
-			_role: 'aim',
-			_course: null,
-			_stewardship: tgl
+			first: this._csv[rows][9].split(' ')[0],
+			last: this._csv[rows][9].split(' ')[1],
+			email: this._csv[rows][8],
+			isNew: null,
+			role: 'aim',
+			course: null,
+			stewardship: tgl
 		};
 
 		// IM OBJECT
 		var im = new OSMPerson({
-			_first: this._csv[rows][11].split(' ')[0],
-			_last: this._csv[rows][11].split(' ')[1],
-			_email: this._csv[rows][10],
-			_isNew: null,
-			_role: 'im',
-			_course: null,
-			_stewardship: aim
+			first: this._csv[rows][11].split(' ')[0],
+			last: this._csv[rows][11].split(' ')[1],
+			email: this._csv[rows][10],
+			isNew: null,
+			role: 'im',
+			course: null,
+			stewardship: aim
 		});
 
 		// OCR OBJECT
 		var ocr = {
-			_first: this._csv[rows][13].split(' ')[0],
-			_last: this._csv[rows][13].split(' ')[1],
-			_email: this._csv[rows][12],
-			_isNew: null,
-			_role: 'ocr',
-			_course: null,
-			_stewardship: inst
+			first: this._csv[rows][13].split(' ')[0],
+			last: this._csv[rows][13].split(' ')[1],
+			email: this._csv[rows][12],
+			isNew: null,
+			role: 'ocr',
+			course: null,
+			stewardship: inst
 		};
 
 		// OCRM OBJECT
 		var ocrm = new OSMPerson({
-			_first: this._csv[rows][14].split(' ')[0],
-			_last: this._csv[rows][14].split(' ')[1],
-			_email: this._csv[rows][15],
-			_isNew: null,
-			_role: 'ocrm',
-			_course: null,
-			_stewardship: ocr
+			first: this._csv[rows][14].split(' ')[0],
+			last: this._csv[rows][14].split(' ')[1],
+			email: this._csv[rows][15],
+			isNew: null,
+			role: 'ocrm',
+			course: null,
+			stewardship: ocr
 		});
 
 		this.addImToOrg(im);
@@ -2417,29 +2415,29 @@ SemesterSetup.prototype.addImToOrg = function(im){
 	}
 	else{
 		for (var i = 0; i < this._org.IM.length; i++){ // IM LEVEL
-			if (this._org.IM[i]._email == im._email){
-				for (var a = 0; a < this._org.IM[i]._stewardship.length; a++){ // AIM LEVEL
-					if (this._org.IM[i]._stewardship[a]._email == im._stewardship[0]._email){
-						for (var t = 0; t < this._org.IM[i]._stewardship[a]._stewardship.length; t++){ // TGL LEVEL
-							if (this._org.IM[i]._stewardship[a]._stewardship[t]._email == im._stewardship[0]._stewardship[0]._email){
-								for (var l = 0; l < this._org.IM[i]._stewardship[a]._stewardship[t]._stewardship.length; l++){ // INSTRUCTOR LEVEL
-									if (this._org.IM[i]._stewardship[a]._stewardship[t]._stewardship[l]._email == im._stewardship[0]._stewardship[0]._stewardship[0]._email){
+			if (this._org.IM[i].email == im.email){
+				for (var a = 0; a < this._org.IM[i].stewardship.length; a++){ // AIM LEVEL
+					if (this._org.IM[i].stewardship[a].email == im.stewardship[0].email){
+						for (var t = 0; t < this._org.IM[i].stewardship[a].stewardship.length; t++){ // TGL LEVEL
+							if (this._org.IM[i].stewardship[a].stewardship[t].email == im.stewardship[0].stewardship[0].email){
+								for (var l = 0; l < this._org.IM[i].stewardship[a].stewardship[t].stewardship.length; l++){ // INSTRUCTOR LEVEL
+									if (this._org.IM[i].stewardship[a].stewardship[t].stewardship[l].email == im.stewardship[0].stewardship[0].stewardship[0].email){
 										return;
 									}
 								}
-								this._org.IM[i]._stewardship[a]._stewardship[t]._stewardship.push(new OSMPerson(im._stewardship[0]._stewardship[0]._stewardship[0]));
+								this._org.IM[i].stewardship[a].stewardship[t].stewardship.push(new OSMPerson(im.stewardship[0].stewardship[0].stewardship[0])); // ADD INST
 								return;
 							}
 						}
-						this._org.IM[i]._stewardship[a]._stewardship.push(new OSMPerson(im._stewardship[0]._stewardship[0]));
+						this._org.IM[i].stewardship[a].stewardship.push(new OSMPerson(im.stewardship[0].stewardship[0])); // ADD TGL
 						return;
 					}
 				}
-				this._org.IM[i]._stewardship.push(new OSMPerson(im._stewardship[0]));
+				this._org.IM[i].stewardship.push(new OSMPerson(im.stewardship[0])); // ADD AIM
 				return;
 			}
 		}
-		this._org.IM.push(im);
+		this._org.IM.push(im); // ADD IM
 	}
 }
 
@@ -2448,9 +2446,20 @@ SemesterSetup.prototype.addOcrmToOrg = function(ocrm){
 		this._org.OCRM.push(ocrm);
 	}
 	else{
-		for (var i = 0; i < this._org.OCRM.length; i++){
-			
+		for (var m = 0; m < this._org.OCRM.length; m++){ // OCRM LEVEL
+			if (this._org.OCRM[m].email == ocrm.email){
+				for (var o = 0; o < this._org.OCRM[m].stewardship.length; o++){ // OCR LEVEL
+					if (this._org.OCRM[m].stewardship[o].email == ocrm.stewardship[0].email){
+						for (var i = 0; this._org.OCRM[m].stewardship[o].stewardship.ength; i++){ // INST LEVEL
+
+						}
+						this._org.OCRM[m].stewardship[o].stewardship.push(new OSMPerson(ocrm.stewardship[0])); // ADD INST
+					}
+				}
+				this._org.OCRM[m].stewardship.push(new OSMPerson(ocrm.stewardship[0])); // ADD OCR
+			}
 		}
+		this._org.OCRM.push(ocrm); // ADD OCRM
 	}
 }
 
