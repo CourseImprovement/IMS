8a8efc332acc1e7c63ea6719c2c54a8e07c06f3d
Lonestar421 <grantjsheridan@gmail.com>
2015-11-20 10:05:45 -0700

    semestersetup

--- build/admin.js
+++ build/admin.js
@@ -2381,7 +2381,7 @@ SemesterSetup.prototype.semesterSetup = function(){
 }
 
 String.prototype.formalize = function(){
-	if (this == undefined || this == null) return;
+	if (this == undefined || this == null || this.length == 0) return;
 	return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 }
 
